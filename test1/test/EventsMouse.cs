c81d64cf0f012639566dcec7c517f83a1a7c2e2c
FylyppS <164223078+FylyppS@users.noreply.github.com>
2024-04-13 12:36:44 +0200

    Add files via upload

---
+++ test1/test/EventsMouse.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows;
+
+namespace test
+{
+    public partial class MainWindow : Window
+    {
+        Point dragStart = new Point(0, 0);
+
+        private void OnMouseScroll(object sender, MouseWheelEventArgs e)
+        {
+            var element = (UIElement)sender;
+
+            if (Keyboard.IsKeyDown(Key.LeftShift))
+            {   // Change order of elements
+                if (e.Delta > 0)
+                    Canvas.SetZIndex(element, Canvas.GetZIndex(element) + 1);
+                else
+                    Canvas.SetZIndex(element, Canvas.GetZIndex(element) - 1);
+
+                Console.WriteLine("ZIndex of focused element: " + Canvas.GetZIndex(element));
+            }
+
+            else if (Keyboard.IsKeyDown(Key.LeftCtrl))
+            {   // Rotate
+                var transform = element.RenderTransform as TransformGroup;
+                RotateTransform child = (RotateTransform)transform.Children[0];
+
+                var angleChange = e.Delta * 0.05;
+                child.Angle += angleChange;
+            }
+
+            else
+            {   // Scale
+                var transform = element.RenderTransform as TransformGroup;
+                ScaleTransform child = (ScaleTransform)transform.Children[1];
+
+                var scaleChange = 0.05F;
+                if (e.Delta > 0)
+                {
+                    child.ScaleX += scaleChange;
+                    child.ScaleY += scaleChange;
+                }
+                else
+                {
+                    child.ScaleX -= scaleChange;
+                    child.ScaleY -= scaleChange;
+                }
+            }
+        }
+
+        private void OnMouseDownTextBlock(object sender, MouseButtonEventArgs e)
+        {
+            SelectedTextBlock = (TextBlock)sender;
+            cardText.Text = SelectedTextBlock.Text;
+
+            OnMouseDown(sender, e);
+        }
+
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            var element = (UIElement)sender;
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {   // Save position for calculations during drag
+                dragStart = e.GetPosition(element);
+                element.CaptureMouse();
+            }
+            else if (e.RightButton == MouseButtonState.Pressed)
+            {   // Reset element position
+                var transform = element.RenderTransform as TransformGroup;
+                TranslateTransform child = (TranslateTransform)transform.Children[2];
+
+                child.X = 0;
+                child.Y = 0;
+            }
+        }
+
+        private void OnMouseUp(object sender, MouseEventArgs e)
+        {
+            var element = (UIElement)sender;
+
+            // Starting position is no longer needed
+            dragStart = new Point(0, 0);
+
+            element.ReleaseMouseCapture();
+        }
+
+        private void OnMouseDrag(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {   // Move / Pan (gets buggy when close to edges of image)
+                var element = (UIElement)sender;
+                var p2 = e.GetPosition(imageSpace);
+
+                var transform = element.RenderTransform as TransformGroup;
+                TranslateTransform child = (TranslateTransform)transform.Children[2];
+
+                child.X = p2.X - dragStart.X;
+                child.Y = p2.Y - dragStart.Y;
+            }
+        }
+
+        #region InkCanvas
+
+        private void InkCanvas_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Rozpocznij mazanie gumką
+            Mouse.OverrideCursor = Cursors.Cross;
+            inkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+        }
+
+        private void InkCanvas_PreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            // Zakończ mazanie gumką
+            Mouse.OverrideCursor = Cursors.Pen;
+            inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+        }
+
+        #endregion
+    }
+}
---
+++ test1/test/ImageElements.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
+using Microsoft.Win32;
+using System.Reflection;
+
+namespace test
+{
+    public partial class MainWindow : Window
+    {
+        #region Background
+
+        private void BtnOpenFile_back_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "JPG Images |*.jpg;*.jpeg|" +
+                        "PNG Images |*.png|" +
+                        "TIFF Images |*.tif|" +
+                        "PDF Images |*.pdf|" +
+                        "All Images |*.png;*.jpg;*.jpeg;*.tif;*pdf"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+                LoadBackgroundImage(fileName);
+            }
+        }
+
+        private void LoadBackgroundImage(string imageName)
+        {
+            if (imageName == null)
+            {
+                back.Source = new BitmapImage(new Uri(@"pack://application:,,,/Pictures/back0.png"));
+            }
+            else
+            {
+                if (Uri.TryCreate(imageName, UriKind.Absolute, out Uri imageUri))
+                {   // Treat imageName as URI
+                    back.Source = new BitmapImage(imageUri);
+                }
+                else
+                {   // Treat imageName as a resource image
+                    back.Source = new BitmapImage(new Uri($@"pack://application:,,,/Pictures/{imageName}.png"));
+                }
+            }
+        }
+
+        #endregion
+
+        #region Foreground / Frame
+
+        private void BtnOpenFile_frame_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "JPG Images |*.jpg;*.jpeg|" +
+                        "PNG Images |*.png|" +
+                        "TIFF Images |*.tif|" +
+                        "PDF Images |*.pdf|" +
+                        "All Images |*.png;*.jpg;*.jpeg;*.tif;*pdf"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+                LoadFrameImage(fileName);
+            }
+        }
+
+        private void LoadFrameImage(string imageName)
+        {
+            if (imageName == null)
+            {
+                frame.Source = null;
+            }
+            else
+            {
+                if (Uri.TryCreate(imageName, UriKind.Absolute, out Uri imageUri))
+                {   // Treat imageName as URI
+                    frame.Source = new BitmapImage(imageUri);
+                }
+                else
+                {   // Treat imageName as a resource image
+                    frame.Source = new BitmapImage(new Uri($@"pack://application:,,,/Frames/{imageName}.png"));
+                }
+            }
+        }
+
+        #endregion
+
+        #region Elements
+
+        private void BtnOpenFile_elem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "JPG Images |*.jpg;*.jpeg|" +
+                        "PNG Images |*.png|" +
+                        "TIFF Images |*.tif|" +
+                        "PDF Images |*.pdf|" +
+                        "All Images |*.png;*.jpg;*.jpeg;*.tif;*pdf"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+                CreateElement(fileName);
+            }
+        }
+        private Image CreateElement(string imageName)
+        {
+            BitmapImage tempBitmap;
+
+            if (Uri.TryCreate(imageName, UriKind.Absolute, out Uri imageUri))
+            {   // Treat imageName as URI
+                tempBitmap = new BitmapImage(imageUri);
+            }
+            else
+            {   // Treat imageName as a resource image
+                tempBitmap = new BitmapImage(new Uri($@"pack://application:,,,/Elements/{imageName}.png"));
+            }
+
+            Image newElement = new Image();
+
+            newElement.MouseUp += OnMouseUp;
+            newElement.MouseDown += OnMouseDown;
+            newElement.MouseMove += OnMouseDrag;
+            newElement.MouseWheel += OnMouseScroll;
+
+            newElement.Source = tempBitmap;
+
+            TransformGroup newTransformGroup = new TransformGroup();
+            newTransformGroup.Children.Add(new RotateTransform());
+            newTransformGroup.Children.Add(new ScaleTransform());
+            newTransformGroup.Children.Add(new TranslateTransform());
+
+            newElement.RenderTransform = newTransformGroup;
+            newElement.RenderTransformOrigin = new Point(0.5, 0.5);
+
+            Size canvasSize = new Size(500, 500);
+            if (tempBitmap.Width > canvasSize.Width || tempBitmap.Height > canvasSize.Height)
+            {
+                float fitScale = Math.Min((float)(canvasSize.Width / tempBitmap.Width), (float)(canvasSize.Height / tempBitmap.Height));
+
+                newElement.Width = tempBitmap.Width * fitScale;
+                newElement.Height = tempBitmap.Height * fitScale;
+            }
+
+            imageSpace.Children.Add(newElement);
+
+            return newElement;
+        }
+
+        #endregion
+
+        #region Text
+
+        TextBlock SelectedTextBlock = null;            
+
+        private void BtnCreateTextBox_Click(object sender, RoutedEventArgs e)
+        {
+            // Tworzenie nowego pola tekstowego
+            TextBlock nowePoleTekstowe = new TextBlock();
+
+            nowePoleTekstowe.MouseUp += OnMouseUp;
+            nowePoleTekstowe.MouseDown += OnMouseDownTextBlock;
+            nowePoleTekstowe.MouseMove += OnMouseDrag;
+            nowePoleTekstowe.MouseWheel += OnMouseScroll;
+
+            nowePoleTekstowe.Text = "Przykładowy nowy tekst";
+
+            nowePoleTekstowe.FontSize = 22;
+            nowePoleTekstowe.FontFamily = textBlock.FontFamily;
+            nowePoleTekstowe.Foreground = textBlock.Foreground;
+            nowePoleTekstowe.TextWrapping = TextWrapping.Wrap;
+            nowePoleTekstowe.TextAlignment = TextAlignment.Center;
+            nowePoleTekstowe.MaxWidth = 256;
+
+            TransformGroup newTransformGroup = new TransformGroup();
+            newTransformGroup.Children.Add(new RotateTransform());
+            newTransformGroup.Children.Add(new ScaleTransform());
+            newTransformGroup.Children.Add(new TranslateTransform());
+
+            nowePoleTekstowe.RenderTransform = newTransformGroup;
+            nowePoleTekstowe.RenderTransformOrigin = new Point(0.5, 0.5);
+            nowePoleTekstowe.Margin = new Thickness(100, 100, 0, 0);
+
+            imageSpace.Children.Add(nowePoleTekstowe);
+        }
+        
+        #endregion
+    }
+}
