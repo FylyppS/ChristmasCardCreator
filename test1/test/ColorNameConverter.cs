3a34646b63db77182d12f04776ccc9c287ff26d4
FylyppS <164223078+FylyppS@users.noreply.github.com>
2024-04-14 18:45:27 +0200

    Add files via upload

---
+++ test1/test/ColorNameConverter.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace test
+{
+    public class ColorNameConverter : IValueConverter
+    {
+        // Metoda konwertująca wartość
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Sprawdź, czy wartość jest niepusta i czy jest to string
+            if (value != null && value is string colorName)
+            {
+                string translatedColorName = TranslateColorName(colorName);
+
+                return translatedColorName;
+            }
+
+            // Jeśli wartość jest null lub nie jest stringiem, zwróć pustą wartość
+            return string.Empty;
+        }
+
+        // Metoda odwrotna konwertująca wartość (niepotrzebna dla tego przypadku, więc pozostawiamy pustą)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        //private string GetTranslatedColorName(string colorName)
+        //{
+        //    // Spróbuj pobrać tłumaczenie koloru z pliku Resources
+        //    string translatedColorName = Properties.Resources.ResourceManager.GetString(colorName, null);
+
+        //    // Jeśli nie ma tłumaczenia, zwróć oryginalną nazwę koloru
+        //    return translatedColorName;
+        //}
+
+        // Metoda do tłumaczenia nazwy koloru
+        private string TranslateColorName(string colorName)
+        {
+            switch (colorName)
+            {
+                case "AliceBlue":
+                    return "Błękit Alicji";
+                case "AntiqueWhite":
+                    return "Biel antyczna";
+                case "Aqua":
+                    return "Turkusowy";
+                case "Aquamarine":
+                    return "Akwamaryna";
+                case "Azure":
+                    return "Błękitny";
+                case "Beige":
+                    return "Beżowy";
+                case "Bisque":
+                    return "Biszkoptowy";
+                case "Black":
+                    return "Czarny";
+                case "BlanchedAlmond":
+                    return "Migdałowy";
+                case "Blue":
+                    return "Niebieski";
+                case "BlueViolet":
+                    return "Fioletowoniebieski";
+                case "Brown":
+                    return "Brązowy";
+                case "BurlyWood":
+                    return "Drewno jesionowe";
+                case "CadetBlue":
+                    return "Błękit kadetowy";
+                case "Chartreuse":
+                    return "Chartreuse";
+                case "Chocolate":
+                    return "Czekoladowy";
+                case "Coral":
+                    return "Koralowy";
+                case "CornflowerBlue":
+                    return "Chabrowy";
+                case "Cornsilk":
+                    return "Kukurydziany";
+                case "Crimson":
+                    return "Szkarłatny";
+                case "Cyan":
+                    return "Cyjan";
+                case "DarkBlue":
+                    return "Ciemnoniebieski";
+                case "DarkCyan":
+                    return "Ciemnocyjanowy";
+                case "DarkGoldenrod":
+                    return "Ciemnozłoty";
+                case "DarkGray":
+                    return "Ciemnoszary";
+                case "DarkGreen":
+                    return "Ciemnozielony";
+                case "DarkKhaki":
+                    return "Ciemne khaki";
+                case "DarkMagenta":
+                    return "Ciemna magenta";
+                case "DarkOliveGreen":
+                    return "Ciemnozielony oliwkowy";
+                case "DarkOrange":
+                    return "Ciemnopomarańczowy";
+                case "DarkOrchid":
+                    return "Ciemnofiołkowy";
+                case "DarkRed":
+                    return "Ciemnoczerwony";
+                case "DarkSalmon":
+                    return "Ciemny łososiowy";
+                case "DarkSeaGreen":
+                    return "Ciemnozielony morski";
+                case "DarkSlateBlue":
+                    return "Ciemnogranatowy";
+                case "DarkSlateGray":
+                    return "Ciemnografitowy";
+                case "DarkTurquoise":
+                    return "Ciemnoturkusowy";
+                case "DarkViolet":
+                    return "Ciemnofioletowy";
+                case "DeepPink":
+                    return "Głęboki róż";
+                case "DeepSkyBlue":
+                    return "Głęboki błękit nieba";
+                case "DimGray":
+                    return "Przygaszony szary";
+                case "DodgerBlue":
+                    return "Dodger blue";
+                case "Firebrick":
+                    return "Ceglasty";
+                case "FloralWhite":
+                    return "Biały kwiatowy";
+                case "ForestGreen":
+                    return "Zielony leśny";
+                case "Fuchsia":
+                    return "Fuksja";
+                case "Gainsboro":
+                    return "Gainsboro";
+                case "GhostWhite":
+                    return "GhostWhite";
+                case "Gold":
+                    return "Złoty";
+                case "Goldenrod":
+                    return "Złotawy";
+                case "Gray":
+                    return "Szary";
+                case "Green":
+                    return "Zielony";
+                case "GreenYellow":
+                    return "Zielonożółty";
+                case "Honeydew":
+                    return "Miodowy";
+                case "HotPink":
+                    return "Gorący róż";
+                case "IndianRed":
+                    return "Indyjski czerwony";
+                case "Indigo":
+                    return "Indygo";
+                case "Ivory":
+                    return "Kość słoniowa";
+                case "Khaki":
+                    return "Khaki";
+                case "Lavender":
+                    return "Lawendowy";
+                case "LavenderBlush":
+                    return "Lawendowy róż";
+                case "LawnGreen":
+                    return "Zielony trawnikowy";
+                case "LemonChiffon":
+                    return "Cytrynowy";
+                case "LightBlue":
+                    return "Jasnoniebieski";
+                case "LightCoral":
+                    return "Jasny koralowy";
+                case "LightCyan":
+                    return "Jasny cyjan";
+                case "LightGoldenrodYellow":
+                    return "Jasny złotawy";
+                case "LightGray":
+                    return "Jasnoszary";
+                case "LightGreen":
+                    return "Jasnozielony";
+                case "LightPink":
+                    return "Jasnoróżowy";
+                case "LightSalmon":
+                    return "Jasny łososiowy";
+                case "LightSeaGreen":
+                    return "Jasny morski zielony";
+                case "LightSkyBlue":
+                    return "Jasny błękit nieba";
+                case "LightSlateGray":
+                    return "Jasny grafitowy";
+                case "LightSteelBlue":
+                    return "Jasny stalowy niebieski";
+                case "LightYellow":
+                    return "Jasnożółty";
+                case "Lime":
+                    return "Limonkowy";
+                case "LimeGreen":
+                    return "Limonkowy zielony";
+                case "Linen":
+                    return "Lniany";
+                case "Magenta":
+                    return "Magenta";
+                case "Maroon":
+                    return "Kasztanowy";
+                case "MediumAquamarine":
+                    return "Średnia akwamaryna";
+                case "MediumBlue":
+                    return "Średni niebieski";
+                case "MediumOrchid":
+                    return "Średni fiołkowy";
+                case "MediumPurple":
+                    return "Średni fioletowy";
+                case "MediumSeaGreen":
+                    return "Średni zielonomorski";
+                case "MediumSlateBlue":
+                    return "Średni grafitowoniebieski";
+                case "MediumSpringGreen":
+                    return "Średni wiosenny zielony";
+                case "MediumTurquoise":
+                    return "Średni turkusowy";
+                case "MediumVioletRed":
+                    return "Średni fiołkowoczerwony";
+                case "MidnightBlue":
+                    return "Granatowy";
+                case "MintCream":
+                    return "Miętowy";
+                case "MistyRose":
+                    return "Mglisty róż";
+                case "Moccasin":
+                    return "Mokasynowy";
+                case "NavajoWhite":
+                    return "Biel Navajo";
+                case "Navy":
+                    return "Granatowy morski";
+                case "OldLace":
+                    return "OldLace";
+                case "Olive":
+                    return "Oliwkowy";
+                case "OliveDrab":
+                    return "Oliwkowy zielony";
+                case "Orange":
+                    return "Pomarańczowy";
+                case "OrangeRed":
+                    return "Czerwony pomarańczowy";
+                case "Orchid":
+                    return "Fiołkowy";
+                case "PaleGoldenrod":
+                    return "Blady złotawy";
+                case "PaleGreen":
+                    return "Blady zielony";
+                case "PaleTurquoise":
+                    return "Blady turkusowy";
+                case "PaleVioletRed":
+                    return "Blady fioletowoczerwony";
+                case "PapayaWhip":
+                    return "Kolba papai";
+                case "PeachPuff":
+                    return "Brzoskwiniowy";
+                case "Peru":
+                    return "Peruwiański";
+                case "Pink":
+                    return "Różowy";
+                case "Plum":
+                    return "Śliwkowy";
+                case "PowderBlue":
+                    return "Proszkowy niebieski";
+                case "Purple":
+                    return "Fioletowy";
+                case "Red":
+                    return "Czerwony";
+                case "RosyBrown":
+                    return "Różowy brąz";
+                case "RoyalBlue":
+                    return "Królewski niebieski";
+                case "SaddleBrown":
+                    return "Brąz siodłowy";
+                case "Salmon":
+                    return "Łososiowy";
+                case "SandyBrown":
+                    return "Piaskowy brąz";
+                case "SeaGreen":
+                    return "Zielonomorski";
+                case "SeaShell":
+                    return "Muszelkowy";
+                case "Sienna":
+                    return "Sienny";
+                case "Silver":
+                    return "Srebrny";
+                case "SkyBlue":
+                    return "Błękit nieba";
+                case "SlateBlue":
+                    return "Grafitowoniebieski";
+                case "SlateGray":
+                    return "Grafitowy";
+                case "Snow":
+                    return "Śnieżny";
+                case "SpringGreen":
+                    return "Wiosenny zielony";
+                case "SteelBlue":
+                    return "Stalowy niebieski";
+                case "Tan":
+                    return "Tan";
+                case "Teal":
+                    return "Zielonomodry";
+                case "Thistle":
+                    return "Karczochowy";
+                case "Tomato":
+                    return "Pomidorowy";
+                case "Transparent":
+                    return "Przezroczysty";
+                case "Turquoise":
+                    return "Turkusowy";
+                case "Violet":
+                    return "Fiolet";
+                case "Wheat":
+                    return "Pszeniczny";
+                case "White":
+                    return "Biały";
+                case "WhiteSmoke":
+                    return "Biały dym";
+                case "Yellow":
+                    return "Żółty";
+                case "YellowGreen":
+                    return "Zielonożółty";
+                default:
+                    return colorName; // Zwróć oryginalną wartość, jeśli nie ma tłumaczenia
+            }
+
+        }
+    }
+}
---
+++ test1/test/MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.Win32;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using QRCoder;
+using System;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
+using static QRCoder.PayloadGenerator;
+
+namespace test
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Load all installed fonts
+            using (InstalledFontCollection col = new InstalledFontCollection())
+            {
+                foreach (var font in col.Families)
+                    FontList.Items.Add(font.Name);
+            }
+
+            // Load colors
+            FontColorList.ItemsSource = typeof(Colors).GetProperties();
+            lineColorComboBox.ItemsSource = typeof(Colors).GetProperties();
+
+
+            // Default selections for comboBoxes
+            FontList.SelectedItem = "Arial";
+            
+            FontColorList.SelectedIndex = 7; // Black
+            lineColorComboBox.SelectedIndex = 7; // Black
+
+            cardSize.SelectedIndex = 0; // 1000x1000px
+
+            lineThicknessComboBox.IsEnabled = false;
+            lineColorComboBox.IsEnabled = false;
+
+            LoadBackgroundImage(null);
+            LoadFrameImage(null);
+
+        }
+            
+        #region Save/Open Files and images
+
+        private BitmapSource RenderCanvas(int width, int height)
+        {
+            float scaleRatiox = (float)(width / imageSpace.RenderSize.Width);
+            float scaleRatioy = (float)(height / imageSpace.RenderSize.Width);
+
+            RenderTargetBitmap renderedCanvas = new RenderTargetBitmap(
+                width,
+                height,
+                96d * scaleRatiox,
+                96d * scaleRatioy,
+                PixelFormats.Default
+                );
+
+            // Get rid of margins on picture with the following lines:
+            DrawingVisual dv = new DrawingVisual();
+            using (DrawingContext dc = dv.RenderOpen())
+            {
+                VisualBrush vb = new VisualBrush(imageSpace);
+                dc.DrawRectangle(vb, null, new Rect(new System.Windows.Point(0, 0), imageSpace.RenderSize));
+            }
+
+            renderedCanvas.Render(dv);
+
+            return renderedCanvas;
+        }
+
+        private void BtnSaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            SavePicture();
+        }
+        private string SavePicture()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Obraz JPG|*.jpg|" +
+                        "Obraz PNG|*.png|" +
+                        "Obraz TIFF|*.tif|" +
+                        "Plik PDF|*.pdf"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                // Select file size from ComboBox
+                string selectedSize = (string)((ComboBoxItem)cardSize.SelectedItem).Content;
+                string[] sizeParts = selectedSize.Split('x');
+                int width = int.Parse(sizeParts[0]);
+                int height = int.Parse(sizeParts[1].Split('p')[0]);
+
+                // Render, encode and save the canvas
+                BitmapEncoder pngEncoder = new PngBitmapEncoder();
+                pngEncoder.Frames.Add(BitmapFrame.Create(RenderCanvas(width, height)));
+
+                using (var fs = File.OpenWrite(saveFileDialog.FileName))
+                    pngEncoder.Save(fs);
+                string attachmentFilePath = saveFileDialog.FileName;
+                MessageBox.Show("Zdjęcie zapisane pomyślnie.");
+                return attachmentFilePath;
+            }
+            return null;
+        }
+
+        #endregion
+        #region Extra windows / email
+        private void OpenAdditionalWindow_Click(object sender, RoutedEventArgs e)
+        {
+            tips additionalWindow = new tips();
+            additionalWindow.Show();
+        }
+
+        
+        private void OpenEmailWindow_Click(object sender, RoutedEventArgs e)
+        {
+            email emailWindow = new email();
+            emailWindow.EmailDataSubmitted += EmailDataSubmitted;
+            emailWindow.Show();
+        }
+        private void EmailDataSubmitted(object sender, email.EmailDataEventArgs e)
+        {
+            // Wywołanie metody wysyłającej e-mail w MainWindow, przekazując dane z okna Email
+            SendEmailWithAttachment(e.To, e.Subject, e.Body);
+        }
+        private void SendEmailWithAttachment(string To, string Subject, string Body)
+        {
+            try
+            {
+                using (SmtpClient client = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    client.EnableSsl = true;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.UseDefaultCredentials = false;
+                    client.Credentials = new NetworkCredential("generatorkartek@gmail.com", "vfyu hfyy mrlq jtne");
+                    MailMessage message = new MailMessage();
+                    message.To.Add(To);
+                    message.From = new MailAddress("generatorkartek@gmail.com");
+                    message.Subject = Subject;
+                    message.Body = Body;
+                    
+                    // Dodanie załącznika
+                    Attachment attachment = new Attachment(SavePicture());
+                    message.Attachments.Add(attachment);
+                    client.Send(message);
+                    MessageBox.Show("Twój email został pomyślnie wysłany!", "Email został wysłany", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        #endregion
+    }
+}
